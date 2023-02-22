1dd85c2bf418fdc0c25e2d10fe0405965bbe1301
BELLO Mouhoutassim <mboabello@yahoo.fr>
2023-02-22 09:15:08 +0100

    intialisation des models

---
+++ collect_app/src/main/java/org/odk/collect/android/utilities/TimeAgo.java
@@ -0,0 +1,32 @@
+package org.odk.collect.android.utilities;
+
+public class TimeAgo {
+    public static final long SECOND_MILLIS = 1000;
+    public static final long MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    public static final long HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    public static final long DAY_MILLIS = 24 * HOUR_MILLIS;
+
+    public static String getTimeAgo(long time) {
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "maintenant";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "il y a une minute";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return "il y a " + diff / MINUTE_MILLIS + " minutes";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "il y a une heure";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return "il y a "+ diff / HOUR_MILLIS + " heures";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "hier";
+        } else {
+            return "il y a " + diff / DAY_MILLIS + " jours";
+        }
+    }
+}
