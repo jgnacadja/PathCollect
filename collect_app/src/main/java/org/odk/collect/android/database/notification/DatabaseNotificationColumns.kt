725598dc50b98cad17aef6c15467deef09d2d749
pedro <freitasferre@gmail.com>
2023-03-01 16:38:17 +0100

    feat: [DSSC#8669mcdrw] Integration de la page de Notification

---
+++ collect_app/src/main/java/org/odk/collect/android/database/notification/DatabaseNotificationColumns.kt
@@ -0,0 +1,14 @@
+package org.odk.collect.android.database.notification
+
+import android.provider.BaseColumns
+
+/**
+ * Columns for the Notifications table.
+ */
+object DatabaseNotificationColumns : BaseColumns {
+    // These are the only things needed for an insert
+    const val TITLE = "title"
+    const val BODY = "body" // can be null
+    const val TIMESTAMP = "timestamp"
+
+}
