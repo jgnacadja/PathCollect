e9a3d518a50862ad21fae264024f7c9e5af916c2
pedro <freitasferre@gmail.com>
2023-03-20 15:04:23 +0100

    feat: [DSSC#8669mcdrw] stockage du notification en local

--- collect_app/src/main/java/org/odk/collect/android/database/notification/NotificationDatabaseMigrator.java
+++ collect_app/src/main/java/org/odk/collect/android/database/notification/NotificationDatabaseMigrator.java
@@ -7,41 +7,23 @@ import org.odk.collect.android.database.DatabaseMigrator;
 import org.odk.collect.android.utilities.SQLiteUtils;
 
 import static android.provider.BaseColumns._ID;
-import static org.odk.collect.android.database.DatabaseConstants.FORMS_TABLE_NAME;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.AUTO_DELETE;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.AUTO_SEND;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.BASE64_RSA_PUBLIC_KEY;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.DATE;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.DELETED_DATE;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.DESCRIPTION;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.DISPLAY_NAME;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.DISPLAY_SUBTEXT;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.FORM_FILE_PATH;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.FORM_MEDIA_PATH;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.GEOMETRY_XPATH;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.JRCACHE_FILE_PATH;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.JR_FORM_ID;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.JR_VERSION;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.LANGUAGE;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.MD5_HASH;
-import static org.odk.collect.android.database.forms.DatabaseFormColumns.SUBMISSION_URI;
+import static org.odk.collect.android.database.DatabaseConstants.NOTIFICATIONS_TABLE_NAME;
+import static org.odk.collect.android.database.notification.DatabaseNotificationColumns.BODY;
+import static org.odk.collect.android.database.notification.DatabaseNotificationColumns.TIMESTAMP;
+import static org.odk.collect.android.database.notification.DatabaseNotificationColumns.TITLE;
 
 public class NotificationDatabaseMigrator implements DatabaseMigrator {
 
-    private static final String[] COLUMN_NAMES_V7 = {_ID, DISPLAY_NAME, DESCRIPTION,
-            JR_FORM_ID, JR_VERSION, MD5_HASH, DATE, FORM_MEDIA_PATH, FORM_FILE_PATH, LANGUAGE,
-            SUBMISSION_URI, BASE64_RSA_PUBLIC_KEY, JRCACHE_FILE_PATH, AUTO_SEND, AUTO_DELETE,
-            "lastDetectedFormVersionHash"};
+    private static final String[] COLUMN_NAMES = {_ID, TITLE,
+            BODY, TIMESTAMP};
 
-    // These exist in database versions 2 and 3, but not in 4...
-    private static final String TEMP_FORMS_TABLE_NAME = "forms_v4";
+    private static final String TEMP_NOTIFICATION_TABLE_NAME = "notifications_v4";
     private static final String MODEL_VERSION = "modelVersion";
 
     public void onCreate(SQLiteDatabase db) {
-        createFormsTableV11(db);
+        createNotificationsTableV11(db);
     }
 
-    @SuppressWarnings({"checkstyle:FallThrough"})
     public void onUpgrade(SQLiteDatabase db, int oldVersion) throws SQLException {
         switch (oldVersion) {
             case 1:
@@ -67,72 +49,35 @@ public class NotificationDatabaseMigrator implements DatabaseMigrator {
     }
 
     public void onDowngrade(SQLiteDatabase db) throws SQLException {
-        SQLiteUtils.dropTable(db, FORMS_TABLE_NAME);
-        createFormsTableV11(db);
+        SQLiteUtils.dropTable(db, NOTIFICATIONS_TABLE_NAME);
+        createNotificationsTableV11(db);
     }
-
     private void upgradeToVersion2(SQLiteDatabase db) {
-        SQLiteUtils.dropTable(db, FORMS_TABLE_NAME);
+        SQLiteUtils.dropTable(db, NOTIFICATIONS_TABLE_NAME);
         onCreate(db);
     }
 
     private void upgradeToVersion4(SQLiteDatabase db, int oldVersion) {
         // adding BASE64_RSA_PUBLIC_KEY and changing type and name of
         // integer MODEL_VERSION to text VERSION
-        SQLiteUtils.dropTable(db, TEMP_FORMS_TABLE_NAME);
-        createFormsTableV4(db, TEMP_FORMS_TABLE_NAME);
+        SQLiteUtils.dropTable(db, TEMP_NOTIFICATION_TABLE_NAME);
+        createNotificationsTableV4(db, TEMP_NOTIFICATION_TABLE_NAME);
         db.execSQL("INSERT INTO "
-                + TEMP_FORMS_TABLE_NAME
+                + TEMP_NOTIFICATION_TABLE_NAME
                 + " ("
                 + _ID
                 + ", "
-                + DISPLAY_NAME
+                + TITLE
                 + ", "
-                + DISPLAY_SUBTEXT
+                + BODY
                 + ", "
-                + DESCRIPTION
-                + ", "
-                + JR_FORM_ID
-                + ", "
-                + MD5_HASH
-                + ", "
-                + DATE
-                + ", " // milliseconds
-                + FORM_MEDIA_PATH
-                + ", "
-                + FORM_FILE_PATH
-                + ", "
-                + LANGUAGE
-                + ", "
-                + SUBMISSION_URI
-                + ", "
-                + JR_VERSION
-                + ", "
-                + ((oldVersion != 3) ? ""
-                : (BASE64_RSA_PUBLIC_KEY + ", "))
-                + JRCACHE_FILE_PATH
+                + TIMESTAMP
                 + ") SELECT "
                 + _ID
                 + ", "
-                + DISPLAY_NAME
+                + TITLE
                 + ", "
-                + DISPLAY_SUBTEXT
-                + ", "
-                + DESCRIPTION
-                + ", "
-                + JR_FORM_ID
-                + ", "
-                + MD5_HASH
-                + ", "
-                + DATE
-                + ", " // milliseconds
-                + FORM_MEDIA_PATH
-                + ", "
-                + FORM_FILE_PATH
-                + ", "
-                + LANGUAGE
-                + ", "
-                + SUBMISSION_URI
+                + BODY
                 + ", "
                 + "CASE WHEN "
                 + MODEL_VERSION
@@ -140,217 +85,116 @@ public class NotificationDatabaseMigrator implements DatabaseMigrator {
                 + "CAST("
                 + MODEL_VERSION
                 + " AS TEXT) ELSE NULL END, "
-                + ((oldVersion != 3) ? ""
-                : (BASE64_RSA_PUBLIC_KEY + ", "))
-                + JRCACHE_FILE_PATH + " FROM "
-                + FORMS_TABLE_NAME);
+                + TIMESTAMP + " FROM "
+                + NOTIFICATIONS_TABLE_NAME);
 
-        // risky failures here...
-        SQLiteUtils.dropTable(db, FORMS_TABLE_NAME);
-        createFormsTableV4(db, FORMS_TABLE_NAME);
+        SQLiteUtils.dropTable(db, NOTIFICATIONS_TABLE_NAME);
+        createNotificationsTableV4(db, NOTIFICATIONS_TABLE_NAME);
         db.execSQL("INSERT INTO "
-                + FORMS_TABLE_NAME
+                + NOTIFICATIONS_TABLE_NAME
                 + " ("
                 + _ID
                 + ", "
-                + DISPLAY_NAME
+                + TITLE
                 + ", "
-                + DISPLAY_SUBTEXT
+                + BODY
                 + ", "
-                + DESCRIPTION
-                + ", "
-                + JR_FORM_ID
-                + ", "
-                + MD5_HASH
-                + ", "
-                + DATE
-                + ", " // milliseconds
-                + FORM_MEDIA_PATH + ", "
-                + FORM_FILE_PATH + ", "
-                + LANGUAGE + ", "
-                + SUBMISSION_URI + ", "
-                + JR_VERSION + ", "
-                + BASE64_RSA_PUBLIC_KEY + ", "
-                + JRCACHE_FILE_PATH + ") SELECT "
+                + TIMESTAMP + ") SELECT "
                 + _ID + ", "
-                + DISPLAY_NAME
+                + TITLE
                 + ", "
-                + DISPLAY_SUBTEXT
+                + BODY
                 + ", "
-                + DESCRIPTION
-                + ", "
-                + JR_FORM_ID
-                + ", "
-                + MD5_HASH
-                + ", "
-                + DATE
+                + TIMESTAMP
                 + ", " // milliseconds
-                + FORM_MEDIA_PATH + ", "
-                + FORM_FILE_PATH + ", "
-                + LANGUAGE + ", "
-                + SUBMISSION_URI + ", "
-                + JR_VERSION + ", "
-                + BASE64_RSA_PUBLIC_KEY + ", "
-                + JRCACHE_FILE_PATH + " FROM "
-                + TEMP_FORMS_TABLE_NAME);
-        SQLiteUtils.dropTable(db, TEMP_FORMS_TABLE_NAME);
+                + NOTIFICATIONS_TABLE_NAME);
+        SQLiteUtils.dropTable(db, NOTIFICATIONS_TABLE_NAME);
     }
 
     private void upgradeToVersion5(SQLiteDatabase db) {
-        SQLiteUtils.addColumn(db, FORMS_TABLE_NAME, AUTO_SEND, "text");
-        SQLiteUtils.addColumn(db, FORMS_TABLE_NAME, AUTO_DELETE, "text");
+        SQLiteUtils.addColumn(db, NOTIFICATIONS_TABLE_NAME, "lastDetectedFormVersionHash", "text");
+        SQLiteUtils.addColumn(db, NOTIFICATIONS_TABLE_NAME, "lastFormVersionHash", "text");
     }
 
     private void upgradeToVersion6(SQLiteDatabase db) {
-        SQLiteUtils.addColumn(db, FORMS_TABLE_NAME, "lastDetectedFormVersionHash", "text");
+        SQLiteUtils.addColumn(db, NOTIFICATIONS_TABLE_NAME, "lastDetectedVersionHash", "text");
     }
 
     private void upgradeToVersion7(SQLiteDatabase db) {
-        String temporaryTable = FORMS_TABLE_NAME + "_tmp";
-        SQLiteUtils.renameTable(db, FORMS_TABLE_NAME, temporaryTable);
-        createFormsTableV7(db);
-        SQLiteUtils.copyRows(db, temporaryTable, COLUMN_NAMES_V7, FORMS_TABLE_NAME);
+        String temporaryTable = NOTIFICATIONS_TABLE_NAME + "_ntn";
+        SQLiteUtils.renameTable(db, NOTIFICATIONS_TABLE_NAME, temporaryTable);
+        createNotificationsTableV7(db);
+        SQLiteUtils.copyRows(db, temporaryTable, COLUMN_NAMES, NOTIFICATIONS_TABLE_NAME);
         SQLiteUtils.dropTable(db, temporaryTable);
     }
 
     private void upgradeToVersion8(SQLiteDatabase db) {
-        SQLiteUtils.addColumn(db, FORMS_TABLE_NAME, GEOMETRY_XPATH, "text");
+        SQLiteUtils.addColumn(db, NOTIFICATIONS_TABLE_NAME, "lastDetectedFormHash", "text");
     }
 
     private void upgradeToVersion9(SQLiteDatabase db) {
-        String temporaryTable = FORMS_TABLE_NAME + "_tmp";
-        SQLiteUtils.renameTable(db, FORMS_TABLE_NAME, temporaryTable);
-        createFormsTableV9(db);
-        SQLiteUtils.copyRows(db, temporaryTable, new String[]{_ID, DISPLAY_NAME, DESCRIPTION,
-                JR_FORM_ID, JR_VERSION, MD5_HASH, DATE, FORM_MEDIA_PATH, FORM_FILE_PATH, LANGUAGE,
-                SUBMISSION_URI, BASE64_RSA_PUBLIC_KEY, JRCACHE_FILE_PATH, AUTO_SEND, AUTO_DELETE,
-                GEOMETRY_XPATH}, FORMS_TABLE_NAME);
+        String temporaryTable = NOTIFICATIONS_TABLE_NAME + "_ntn";
+        SQLiteUtils.renameTable(db, NOTIFICATIONS_TABLE_NAME, temporaryTable);
+        createNotificationsTableV9(db);
+        SQLiteUtils.copyRows(db, temporaryTable, new String[]{_ID, TITLE, BODY,
+                TIMESTAMP}, NOTIFICATIONS_TABLE_NAME);
         SQLiteUtils.dropTable(db, temporaryTable);
     }
 
     private void upgradeToVersion10(SQLiteDatabase db) {
-        String temporaryTable = FORMS_TABLE_NAME + "_tmp";
-        SQLiteUtils.renameTable(db, FORMS_TABLE_NAME, temporaryTable);
-        createFormsTableV10(db);
-        SQLiteUtils.copyRows(db, temporaryTable, new String[]{_ID, DISPLAY_NAME, DESCRIPTION,
-                JR_FORM_ID, JR_VERSION, MD5_HASH, DATE, FORM_MEDIA_PATH, FORM_FILE_PATH, LANGUAGE,
-                SUBMISSION_URI, BASE64_RSA_PUBLIC_KEY, JRCACHE_FILE_PATH, AUTO_SEND, AUTO_DELETE,
-                GEOMETRY_XPATH}, FORMS_TABLE_NAME);
+        String temporaryTable = NOTIFICATIONS_TABLE_NAME + "_ntn";
+        SQLiteUtils.renameTable(db, NOTIFICATIONS_TABLE_NAME, temporaryTable);
+        createNotificationsTableV10(db);
+        SQLiteUtils.copyRows(db, temporaryTable, new String[]{_ID, TITLE, BODY,
+                TIMESTAMP}, NOTIFICATIONS_TABLE_NAME);
         SQLiteUtils.dropTable(db, temporaryTable);
     }
 
     private void upgradeToVersion11(SQLiteDatabase db) {
-        String temporaryTable = FORMS_TABLE_NAME + "_tmp";
-        SQLiteUtils.renameTable(db, FORMS_TABLE_NAME, temporaryTable);
-        createFormsTableV11(db);
-        SQLiteUtils.copyRows(db, temporaryTable, new String[]{_ID, DISPLAY_NAME, DESCRIPTION,
-                JR_FORM_ID, JR_VERSION, MD5_HASH, DATE, FORM_MEDIA_PATH, FORM_FILE_PATH, LANGUAGE,
-                SUBMISSION_URI, BASE64_RSA_PUBLIC_KEY, JRCACHE_FILE_PATH, AUTO_SEND, AUTO_DELETE,
-                GEOMETRY_XPATH, DELETED_DATE}, FORMS_TABLE_NAME);
+        String temporaryTable = NOTIFICATIONS_TABLE_NAME + "_ntn";
+        SQLiteUtils.renameTable(db, NOTIFICATIONS_TABLE_NAME, temporaryTable);
+        createNotificationsTableV11(db);
+        SQLiteUtils.copyRows(db, temporaryTable, new String[]{_ID, TITLE, BODY,
+                TIMESTAMP}, NOTIFICATIONS_TABLE_NAME);
         SQLiteUtils.dropTable(db, temporaryTable);
     }
-
-    private void createFormsTableV4(SQLiteDatabase db, String tableName) {
+    private void createNotificationsTableV4(SQLiteDatabase db, String tableName) {
         db.execSQL("CREATE TABLE IF NOT EXISTS " + tableName + " ("
                 + _ID + " integer primary key, "
-                + DISPLAY_NAME + " text not null, "
-                + DISPLAY_SUBTEXT + " text not null, "
-                + DESCRIPTION + " text, "
-                + JR_FORM_ID + " text not null, "
-                + JR_VERSION + " text, "
-                + MD5_HASH + " text not null, "
-                + DATE + " integer not null, " // milliseconds
-                + FORM_MEDIA_PATH + " text not null, "
-                + FORM_FILE_PATH + " text not null, "
-                + LANGUAGE + " text, "
-                + SUBMISSION_URI + " text, "
-                + BASE64_RSA_PUBLIC_KEY + " text, "
-                + JRCACHE_FILE_PATH + " text not null, "
-                + AUTO_SEND + " text, "
-                + AUTO_DELETE + " text, "
+                + TITLE + " text not null, "
+                + BODY + " text not null, "
+                + TIMESTAMP + " integer, "
                 + "lastDetectedFormVersionHash" + " text);");
     }
 
-    private void createFormsTableV7(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + FORMS_TABLE_NAME + " ("
+    private void createNotificationsTableV7(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + NOTIFICATIONS_TABLE_NAME + " ("
                 + _ID + " integer primary key, "
-                + DISPLAY_NAME + " text not null, "
-                + DESCRIPTION + " text, "
-                + JR_FORM_ID + " text not null, "
-                + JR_VERSION + " text, "
-                + MD5_HASH + " text not null, "
-                + DATE + " integer not null, " // milliseconds
-                + FORM_MEDIA_PATH + " text not null, "
-                + FORM_FILE_PATH + " text not null, "
-                + LANGUAGE + " text, "
-                + SUBMISSION_URI + " text, "
-                + BASE64_RSA_PUBLIC_KEY + " text, "
-                + JRCACHE_FILE_PATH + " text not null, "
-                + AUTO_SEND + " text, "
-                + AUTO_DELETE + " text, "
-                + "lastDetectedFormVersionHash" + " text);");
+                + TITLE + " text not null, "
+                + BODY + " text not null, "
+                + TIMESTAMP + " integer);");
     }
 
-    private void createFormsTableV9(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + FORMS_TABLE_NAME + " ("
+    private void createNotificationsTableV9(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + NOTIFICATIONS_TABLE_NAME + " ("
                 + _ID + " integer primary key, "
-                + DISPLAY_NAME + " text not null, "
-                + DESCRIPTION + " text, "
-                + JR_FORM_ID + " text not null, "
-                + JR_VERSION + " text, "
-                + MD5_HASH + " text not null, "
-                + DATE + " integer not null, " // milliseconds
-                + FORM_MEDIA_PATH + " text not null, "
-                + FORM_FILE_PATH + " text not null, "
-                + LANGUAGE + " text, "
-                + SUBMISSION_URI + " text, "
-                + BASE64_RSA_PUBLIC_KEY + " text, "
-                + JRCACHE_FILE_PATH + " text not null, "
-                + AUTO_SEND + " text, "
-                + AUTO_DELETE + " text, "
-                + GEOMETRY_XPATH + " text, "
+                + TITLE + " text not null, "
+                + BODY + " text, "
+                + TIMESTAMP + " integer, "
                 + "deleted" + " boolean default(0));");
     }
-
-    private void createFormsTableV10(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + FORMS_TABLE_NAME + " ("
+    private void createNotificationsTableV10(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + NOTIFICATIONS_TABLE_NAME + " ("
                 + _ID + " integer primary key, "
-                + DISPLAY_NAME + " text not null, "
-                + DESCRIPTION + " text, "
-                + JR_FORM_ID + " text not null, "
-                + JR_VERSION + " text, "
-                + MD5_HASH + " text not null, "
-                + DATE + " integer not null, " // milliseconds
-                + FORM_MEDIA_PATH + " text not null, "
-                + FORM_FILE_PATH + " text not null, "
-                + LANGUAGE + " text, "
-                + SUBMISSION_URI + " text, "
-                + BASE64_RSA_PUBLIC_KEY + " text, "
-                + JRCACHE_FILE_PATH + " text not null, "
-                + AUTO_SEND + " text, "
-                + AUTO_DELETE + " text, "
-                + GEOMETRY_XPATH + " text, "
-                + DELETED_DATE + " integer);");
+                + TITLE + " text not null, "
+                + BODY + " text, "
+                + TIMESTAMP + " integer);");
     }
-
-    private void createFormsTableV11(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + FORMS_TABLE_NAME + " ("
+    private void createNotificationsTableV11(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + NOTIFICATIONS_TABLE_NAME + " ("
                 + _ID + " integer primary key, "
-                + DISPLAY_NAME + " text not null, "
-                + DESCRIPTION + " text, "
-                + JR_FORM_ID + " text not null, "
-                + JR_VERSION + " text, "
-                + MD5_HASH + " text not null UNIQUE ON CONFLICT IGNORE, "
-                + DATE + " integer not null, " // milliseconds
-                + FORM_MEDIA_PATH + " text not null, "
-                + FORM_FILE_PATH + " text not null, "
-                + LANGUAGE + " text, "
-                + SUBMISSION_URI + " text, "
-                + BASE64_RSA_PUBLIC_KEY + " text, "
-                + JRCACHE_FILE_PATH + " text not null, "
-                + AUTO_SEND + " text, "
-                + AUTO_DELETE + " text, "
-                + GEOMETRY_XPATH + " text, "
-                + DELETED_DATE + " integer);");
+                + TITLE + " text not null, "
+                + BODY + " text, "
+                + TIMESTAMP + " integer);");
     }
 }
