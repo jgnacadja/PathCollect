8cb84ba5ca662d01513ed6c96cef76e4241faad9
BELLO Mouhoutassim <mboabello@yahoo.fr>
2023-06-20 23:14:37 +0200

    Ajout d'exception https rame

--- collect_app/src/main/java/org/odk/collect/android/tasks/SendRequestToWP.java
+++ collect_app/src/main/java/org/odk/collect/android/tasks/SendRequestToWP.java
@@ -5,6 +5,13 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -18,15 +25,20 @@ public class SendRequestToWP extends AsyncTask<String, Void, ResponseBody> {
 
     @SuppressLint("TimberExceptionLogging")
     protected ResponseBody doInBackground(String... urls) {
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = getHttpClient();
         Request request = new Request.Builder()
                 .url(urls[0])
                 .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            return response.body();
-        } catch (IOException e) {
-            Timber.tag("SendRequestToWP").d(e.getMessage());
+        if (client != null) {
+            try (Response response = client.newCall(request).execute()) {
+                return response.body();
+            } catch (IOException e) {
+                Timber.tag("SendRequestToWP").d(e.getMessage());
+                return null;
+            }
+        } else {
+            Timber.tag("SendRequestToWP").d("Client is null");
             return null;
         }
     }
@@ -35,4 +47,37 @@ public class SendRequestToWP extends AsyncTask<String, Void, ResponseBody> {
         // TODO: check this.exception
         // TODO: do something with the feed
     }
+
+    private OkHttpClient getHttpClient() {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new java.security.cert.X509Certificate[]{};
+                    }
+                }
+        };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            OkHttpClient.Builder newBuilder = new OkHttpClient.Builder();
+            newBuilder.sslSocketFactory(sc.getSocketFactory(), (X509TrustManager) trustAllCerts[0]);
+            newBuilder.hostnameVerifier((hostname, session) -> true);
+            return newBuilder.build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
